87e28a5014be66b4fc06badcdab25f1f48b4ae1e
Alexander Volosnikov <aleks_andr83@mail.ru>
2022-02-26 14:05:56 +0300

    Выполнение задания №2 1й части обучения

---
+++ Assets/Scripts/Hero/Hero.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    /// <summary>
+    /// Вектор движения
+    /// </summary>
+    private Vector2 _direction;
+
+    /// <summary>
+    /// Скорость передвижения
+    /// </summary>
+    [SerializeField] private float _speed = 4;
+
+    /// <summary>
+    /// Задание вектора движения героя
+    /// </summary>
+    /// <param name="direction"></param>
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction;
+    }
+
+    private void Update()
+    {
+        if (_direction.x != 0 || _direction.y != 0)
+        {
+            var delta = _direction * _speed * Time.deltaTime;
+            var newXpos = transform.position.x + delta.x;
+            var newYpos = transform.position.y + delta.y;
+
+            transform.position = new Vector3(newXpos, newYpos, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Hero/HeroInputReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+            var vector = context.ReadValue<Vector2>();
+            _hero.SetDirection(vector);
+    }
+}
