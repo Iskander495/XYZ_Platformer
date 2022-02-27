42eceaccb0df9a01de38a2eea04c3f7eb355e616
Alexander Volosnikov <aleks_andr83@mail.ru>
2022-02-27 16:54:01 +0300

    + прыжок на space + слежение камеры за персонажем (пока кривое)

---
+++ Assets/Scripts/Camera/FollowToPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowToPlayer : MonoBehaviour
+{
+    /// <summary>
+    /// Объект, за которым будет следить камера
+    /// </summary>
+    [SerializeField] private GameObject _targetObject;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(_targetObject.transform.position.x, transform.position.y, transform.position.z);
+    }    
+}
